55c37486d48f9ffbb994008b81352a6410a55cc0
Jack-Cb <loreleijack2004@gmail.com>
2026-03-08 22:21:49 -0700

    Code Fixes to ADC, Basic Read Working

--- Capstone_Signal_Processing_PCB/Sigproc_Code/Sigproc_Code.ino
+++ Capstone_Signal_Processing_PCB/Sigproc_Code/Sigproc_Code.ino
@@ -1,109 +1,113 @@
 #include <SPI.h>
 
-#define VSPI_MOSI 23
-#define VSPI_MISO 19
-#define VSPI_SCLK 18
-#define VSPI_CS 5
-
-SPIClass * hspi = NULL;
-
-// AD4170 Register Addresses
-
-
-// AD4170 Commands
-#define COMM_WRITE_MASK 0x00
-#define COMM_READ_MASK 0x40
-
-// Function prototypes
-void writeAD4170Register(uint16_t address, uint8_t value);
-long readAD4170Data();
-void AD4170_Init_AIN0_Diff();
+const int chipSelectPin = 5;
 
 void setup() {
+  pinMode(chipSelectPin, OUTPUT);
+  digitalWrite(chipSelectPin, HIGH);
+  SPI.begin();
+  
+  // AD4170 typically uses SPI Mode 3 (CPOL=1, CPHA=1)
+  SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE3));
+  
   Serial.begin(115200);
-  SPI.begin(VSPI_SCLK, VSPI_MISO, VSPI_MOSI, VSPI_CS);
-
-  pinMode(VSPI_CS, OUTPUT);
-  digitalWrite(VSPI_CS, HIGH);
-
-  AD4170_Init_AIN0_Diff();
-
-  Serial.println("AD4170 Initialized for AIN0 Differential Continuous Read.");
-}
-
-void loop() {
-  // Wait for the Data Ready signal to go LOW
-
-  // Data is ready, read the value
-  long adc_value = readAD4170Data();
-
-  //Serial.print("ADC Value: ");
-  delay(10);
-  Serial.println(adc_value);
-}
-
-void writeAD4170Register(uint16_t address, uint8_t value) {
-  SPI.beginTransaction(SPISettings(16000000, MSBFIRST, SPI_MODE3));
-  digitalWrite(VSPI_CS, LOW);
-
-  //SPI.transfer(0b0);
-  //SPI.transfer(0b0);
-  // Send the command byte: WRITE operation + register address
-  /*uint8_t command_first = COMM_WRITE_MASK | ((address >> 8) & 0x3F);
-  if(((address >> 8) & 0x3F) != 0){
-    SPI.transfer(command_first);
-  }
-  uint8_t command_second = (address & 0xFF);
-  SPI.transfer(command_second);
-  */
-  SPI.transfer16((0b0011111111111111 & address));
-  // Send the data byte
-  SPI.transfer(value);
-
-  digitalWrite(VSPI_CS, HIGH);
-  SPI.endTransaction();
-}
-
-long readAD4170Data() {
-  long data = 0;
-  SPI.beginTransaction(SPISettings(16000000, MSBFIRST, SPI_MODE3));
-  digitalWrite(VSPI_CS, LOW);
   
-  byte b1 = SPI.transfer(0x00);
-  byte b2 = SPI.transfer(0x00);
-  byte b3 = SPI.transfer(0x00);
-
-  digitalWrite(VSPI_CS, HIGH);
-  SPI.endTransaction();
-
-  data = (long)b1 << 16 | (long)b2 << 8 | b3;
-
-  // Sign extension for 24-bit value to 32-bit long
-  if (data & 0x800000) {
-    data |= 0xFF000000;
-  }
-  
-  return data;
 }
-
-void AD4170_Init_AIN0_Diff() {
-  SPI.beginTransaction(SPISettings(16000000, MSBFIRST, SPI_MODE3));
-  digitalWrite(VSPI_CS, LOW);
-  for (int i = 0; i < 3; i++) {
-    for (int i = 0; i < 7; i++) {
+// MAKE SURE THAT ALL GROUNDS ARE SHARED/SINGLE GROUND FOR ALL
+void readChipID() {
+  digitalWrite(chipSelectPin, LOW);
+  /*for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 7; j++) {
       SPI.transfer(0xFF); 
     } 
     SPI.transfer(0xFE);
   }
-  digitalWrite(VSPI_CS, HIGH);
-  SPI.endTransaction();
+  */
+  for (int j = 0; j < 7; j++) {
+      SPI.transfer(0xFF); 
+  }
+  SPI.transfer(0xFE);
+
+  digitalWrite(chipSelectPin, HIGH);
+
+  digitalWrite(chipSelectPin, LOW);
+  SPI.transfer(0x01);
+  SPI.transfer(0b10001000);
+  digitalWrite(chipSelectPin, HIGH);
+
+  digitalWrite(chipSelectPin, LOW);
+  SPI.transfer(0x00);
+  SPI.transfer(0x10);
+  digitalWrite(chipSelectPin, HIGH);
+
+  digitalWrite(chipSelectPin, LOW);
+  SPI.transfer(0x10);
+  SPI.transfer(0b00110111);
+  digitalWrite(chipSelectPin, HIGH);
+
+  digitalWrite(chipSelectPin, LOW);
+  SPI.transfer(0x01);
+  SPI.transfer(0b10000000);
+  digitalWrite(chipSelectPin, HIGH);
+
+  digitalWrite(chipSelectPin, LOW);
+  SPI.transfer16(0b0000000001110001);
+  SPI.transfer(0b00000000);
+  SPI.transfer(0b10000000); // PROG CONT READ
+  digitalWrite(chipSelectPin, HIGH);
+
+  digitalWrite(chipSelectPin, LOW);
+  SPI.transfer16(0b0000000001111001);
+  SPI.transfer(0b00000000);
+  SPI.transfer(0b00000001);
+  digitalWrite(chipSelectPin, HIGH);
+
+  digitalWrite(chipSelectPin, LOW);
+  SPI.transfer16(0b0000000010000011);
+  SPI.transfer(0b00000000);
+  SPI.transfer(0b00011000); // DGND
+  digitalWrite(chipSelectPin, HIGH);
+
+  digitalWrite(chipSelectPin, LOW);
+  SPI.transfer16(0b0000000011000011);
+  SPI.transfer(0b00000000);
+  SPI.transfer(0b01100000); // DGND
+  digitalWrite(chipSelectPin, HIGH);
+
+  // 0x40 (Read bit) | 0x03 (Register address) = 0x43
+  digitalWrite(chipSelectPin, LOW);
+  SPI.transfer16(0b0100000000000011); 
+  // Read the 16-bit ID value
+  uint8_t msb = SPI.transfer(0x00);
+  uint8_t lsb = SPI.transfer(0x00);
   
-  delay(100); // Wait for reset to complete
-  writeAD4170Register(0x0070, 0x10);
+  digitalWrite(chipSelectPin, HIGH);
+  
+  uint16_t chipID = (msb << 8) | lsb;
+  
+  Serial.print("AD4170 Chip ID: 0x");
+  Serial.println(chipID, HEX);
 
-  writeAD4170Register(0x00C2, 0x60);
+  digitalWrite(chipSelectPin, LOW);
+  SPI.transfer16(0b0100000000100011); 
+  // Read the 16-bit ID value
 
-  writeAD4170Register(0x006A, 0x01);
+  uint32_t data = 0;
 
+  data |= (uint32_t)SPI.transfer(0x00) << 16;
+  data |= (uint32_t)SPI.transfer(0x00) << 8;
+  data |= (uint32_t)SPI.transfer(0x00);
+  
+  //Serial.println(data);
+  float voltage = (data * 5) / 16777216.0;
+  Serial.println(voltage);
+
+
+  digitalWrite(chipSelectPin, HIGH);
+  
+  // Expected ID for AD4170-4 is typically 0xXXXX (Check datasheet for exact silicon revision code)
 }
 
+void loop() {
+  readChipID();
+}
